483cbe57f1fcc47269c7e43cb806e2794ef59995
Hamza Shabir <hamzii2205@gmail.com>
2024-11-09 14:18:29 +0500

    add not found file

---
+++ next-js-portfolio-with-custom-css/portfolio/src/app/not-found.tsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <>
+      <div className="outer-not-found">
+        <div className="inner-not-found">
+          <h1>No One Exist Here..</h1>
+          <h2>404 Error, Not Found..</h2>
+          <a href="home">Go Back</a>
+        </div>
+      </div>
+    </>
+  );
+}
