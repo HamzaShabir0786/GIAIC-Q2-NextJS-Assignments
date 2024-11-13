87d77c01df89fd1b8afc33647e0a953da0e21494
Hamza Shabir <hamzii2205@gmail.com>
2024-11-13 20:11:42 +0500

    change services data from normal to map object

--- next-js-portfolio-with-custom-css/portfolio/src/app/services/page.tsx
+++ next-js-portfolio-with-custom-css/portfolio/src/app/services/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Metadata } from "next";
 import "./services.css";
 import "../globals.css";
+import { dataServices } from "../components/dataForServices/page";
 
 export const metadata: Metadata = {
   title: "Services",
@@ -19,138 +20,29 @@ export default function Services() {
         </p>
 
         <div className="services-grid">
-          <div className="grid-items item-1">
-            <div className="skill-div">
-              <Image
-                src="/image/web dev pic.png"
-                width={60}
-                height={48}
-                alt="it is a skill pic"
-              />
-            </div>
-            <strong>Web-Developer</strong>
-            <br />
-            <p>
-              I’m a passionate web developer. If you’re looking for a unique,
-              fully responsive, and user-friendly website, feel free to contact
-              me..!
-            </p>
-            <button>
-              <Link href="/contact" target="_blank">
-                Buy Service
-              </Link>
-            </button>
-          </div>
-
-          <div className="grid-items item-2">
-            <div className="skill-div">
-              <Image
-                src="/image/ui ux.png"
-                width={60}
-                height={48}
-                alt="it is a skill pic"
-              />
-            </div>
-            <strong>UI UX Designer</strong>
-            <br />
-            <p>
-              I'm a passionate UI UX Designer.If you want to unique, fully
-              actractivness and userfrinedly website so contact me.
-            </p>
-            <button>
-              <Link href="/contact" target="_blank">
-                Buy Service
-              </Link>
-            </button>
-          </div>
-
-          <div className="grid-items item-3">
-            <div className="skill-div">
-              <Image
-                src="/image/SEO.png"
-                width={60}
-                height={48}
-                alt="it is a skill pic"
-              />
-            </div>
-            <strong>SEO</strong>
-            <br />
-            <p>
-              I'm aSEO Developer.If you want to unique, and highly well rank SEO
-              for all browser so contact me.
-            </p>
-            <button>
-              <Link href="/contact" target="_blank">
-                Buy Service
-              </Link>
-            </button>
-          </div>
-
-          <div className="grid-items item-4">
-            <div className="skill-div">
-              <Image
-                src="/image/Graphic.avif"
-                width={60}
-                height={48}
-                alt="it is a skill pic"
-              />
-            </div>
-            <strong>Graphic Designer</strong>
-            <br />
-            <p>
-              I'm a Graphic Designer.If you want to unique, fully actractiv and
-              userfrinedly Graphics so contact me.
-            </p>
-            <button>
-              <Link href="/contact" target="_blank">
-                Buy Service
-              </Link>
-            </button>
-          </div>
-
-          <div className="grid-items item-5">
-            <div className="skill-div">
-              <Image
-                src="/image/video-editing.png"
-                width={60}
-                height={48}
-                alt="it is a skill pic"
-              />
-            </div>
-            <strong>Video Editing</strong>
-            <br />
-            <p>
-              I'm a Video Editor. If you want to unique videos so contact me.My
-              tools for video editing are Adobe premier pro and Filmora.
-            </p>
-            <button>
-              <Link href="/contact" target="_blank">
-                Buy Service
-              </Link>
-            </button>
-          </div>
-
-          <div className="grid-items item-6">
-            <div className="skill-div">
-              <Image
-                src="/image/web-hosting.png"
-                width={60}
-                height={48}
-                alt="it is a skill pic"
-              />
-            </div>
-            <strong>Web Hosting</strong>
-            <br />
-            <p>
-              I'm a Web-Developer as well Hoster.You can hire me as a front-end
-              webdeveloper and hoster.
-            </p>
-            <button>
-              <Link href="/contact" target="_blank">
-                Buy Service
-              </Link>
-            </button>
-          </div>
+          
+        {dataServices?.map((items, index) => {
+            return (
+              <div className="grid-items item-2" key={index}>
+                <div className="skill-div">
+                  <Image
+                    src={items.imageSrc}
+                    width={items.imageWidth}
+                    height={items.imageHeight}
+                    alt={items.imageAlt}
+                  />
+                </div>
+                <strong>{items.strongText}</strong>
+                <br />
+                <p>{items.paragraphText} </p>
+                <button>
+                  <Link href={items.buttonLink} target="_blank">
+                    {items.buttonText}
+                  </Link>
+                </button>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
