60730c20168ec6f669ec2192a8ac8c423147e44d
Hamza Shabir <hamzii2205@gmail.com>
2024-12-05 16:08:52 +0500

    description increase

--- next-js-portfolio-with-custom-css/portfolio/src/app/layout.tsx
+++ next-js-portfolio-with-custom-css/portfolio/src/app/layout.tsx
@@ -10,7 +10,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Portfolio ",
-  description: "it's a my personal portfolio on hamzah.vercel.app  ",
+  description: `it's a my personal portfolio on hamzah.vercel.app  I'm currently a student specializing in Generative AI at GIAIC, Karachi. I possess a diverse set of skills in fields like web development, UI/UX design, video editing,
+     and graphic design. My technical expertise includes HTML, CSS, Tailwind CSS, JavaScript, TypeScript, React.js, Next.js, Python, Pandas and Numpy.God willing, in the future, I will be able to work on AI-based projects.  `,
 };
 
 export default function RootLayout({
